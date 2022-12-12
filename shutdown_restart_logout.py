e3ca80cae833468a2acf4009952c764208066a88
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-12 18:30:07 +0200

    Create shutdown_restart_logout.py

---
+++ shutdown_restart_logout.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import os
+
+# Shutdown, Restart and Logout from the PC
+# user define function
+def shutdown():
+    return os.system('shutdown /s /t 1')
+    
+def restart():
+    return os.system('shutdown /r /t 1')
+    
+def logout():
+    return os.system('shutdown -l')
+
+# tkinter object
+master = Tk()
+
+# background set to grey
+master.configure(bg='light grey')
+
+# creating a button using the widget
+# Buttons that will call the submit function
+Button(master, text="Shutdown", command=shutdown).grid(row=0)
+Button(master, text="Restart", command=restart).grid(row=1)
+Button(master, text="Log out", command=logout).grid(row=2)
