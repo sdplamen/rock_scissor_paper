70de25c0df1eab71ed18c0ddd7290584eaeb224c
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-12 18:34:22 +0200

    Create version_python.py

---
+++ version_python.py
@@ -0,0 +1,7 @@
+import sys
+# sys.version_info(major=3, minor=7, micro=0, releaselevel='final', serial=0)
+
+if sys.version_info.major == 3:
+    print('Your python version is Python 3')
+else:
+    print('Your python version is Python 2')
