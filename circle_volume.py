34739b7fffa798876c19dd9392e3e7843486ef41
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-16 21:40:13 +0200

    Update circle_volume.py

--- circle_volume.py
+++ circle_volume.py
@@ -1,6 +1,7 @@
 import math
 
+def volume(rad):
+    volume = (4 / 3 * math.pi * rad ** 3)
+    print('The volume of circle is : ', round(volume, 2))
 rad = int(input('Type the raduis for the circle : '))
-volume = (4 / 3 * math.pi * rad ** 3)
-
-print('The volume of circle is : ', round(volume, 2))
+volume(rad)
