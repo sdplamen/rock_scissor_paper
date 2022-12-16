c139f4bccef832e996366dd18d24c0763326ac01
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-16 21:12:46 +0200

    Create swap_variables.py

---
+++ swap_variables.py
@@ -0,0 +1,9 @@
+def swap():2
+    z = x1
+    x = y1
+    y = z
+    print('The value of x after swapping:', x)
+    print('The value of y after swapping:', z)
+x1 = input('Type a variable x to swap :')
+y1 = input('Type a variable y to swap :')
+swap()
