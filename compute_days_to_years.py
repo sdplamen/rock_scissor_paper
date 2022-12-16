815ef720d403cb50e6d95a5e3ec4b6159161385a
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-16 21:49:32 +0200

    Update compute_days_to_years.py

--- compute_days_to_years.py
+++ compute_days_to_years.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
+def days(delta):
+# convert string to date object
+    fDate = datetime.strptime(fDate, '%Y/%m/%d')
+    sDate = datetime.strptime(sDate, '%Y/%m/%d')
+    
+    delta = sDate - fDate
+
+    print('The difference between your first and second date is', abs(delta), 'days')
 fDate = input('Type first date in format 1981/12/2 : ')
 sDate = input('Type second date in format 1981/12/2 : ')
-
-# convert string to date object
-fDate = datetime.strptime(fDate, '%Y/%m/%d')
-sDate = datetime.strptime(sDate, '%Y/%m/%d')
-
-delta = sDate - fDate
-print('The difference between your first and second date is', delta, 'days')
