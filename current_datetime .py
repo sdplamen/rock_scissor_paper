983f2acc40f2c23c9e4d1ca81acc860b5263a41a
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-16 21:37:59 +0200

    Update current_datetime .py

--- current_datetime .py
+++ current_datetime .py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
-now = datetime.now()
-print('# 3')
-print('Current date and time is', now)
+def currTime():
+    now = datetime.now()
+    print('Current date and time is', now)
+currTime()
