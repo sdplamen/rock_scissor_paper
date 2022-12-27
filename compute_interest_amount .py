65f08c280bf43ebb7dd1cdda580e77a93ef089c5
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-27 19:00:08 +0200

    Update compute_interest_amount .py

--- compute_interest_amount .py
+++ compute_interest_amount .py
@@ -1,5 +1,9 @@
-amount = int(unput('Type amount :'))
-interest = float(unput('Type interest :'))
-years = int(unput('Type years to pay :'))
-future_value = amount * ((1 + (0.01 * interest)) ** years)
-print(round(future_value, 2))
+# FV = I х (1 + (R х T)) where: I = Investment amount R = Interest rate T = Number of years
+
+def futVal(amount, interest, years):
+    future_value = amount * ((1 + (0.01 * interest)) ** years)
+    print('The future amount is :', round(future_value, 2))
+amount = int(input('Type an amount:'))
+interest = float(input('Type an interest for this amount:'))
+years = int(input('Type how many years for paying amount:'))
+futVal(amount, interest, years)
