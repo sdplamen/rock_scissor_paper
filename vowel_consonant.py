bd7b4c387a97568c11dd0b20dfab076e8c9daa6d
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-27 19:13:07 +0200

    Update vowel_consonant.py

--- vowel_consonant.py
+++ vowel_consonant.py
@@ -1,6 +1,8 @@
+def vowel(vow):
+    vowels = 'AaEeIiOoUu'
+    if vow in vowels:
+        print('It\'s a vowel.')
+    else:
+        print(vow, 'is a consonant.')
 vow = input('Type a vowel or consonant : ')
-vowels = 'AaEeIiOoUu'
-if vow in vowels:
-    print('It\'s a vowel.')
-else:
-    print('It\'s a consonant.')
+vowel(vow)
