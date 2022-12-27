ec1975b3c5a34aff6304fa49902c6211565759c7
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-27 19:12:27 +0200

    Update triangle_area.py

--- triangle_area.py
+++ triangle_area.py
@@ -1,4 +1,6 @@
+def area(b, hB):
+    area = (1 / 2 * hB * b)
+    print('The area of triangle is :', round(area, 2))
 b = int(input('Type the base of triagle : '))
 hB = int(input('Type the heigth of triagle : '))
-area = (1 / 2 * hB * b)
-print('The area of triangle is :', round(area, 2))
+area(b, hB)
