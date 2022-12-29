d3f76abfdbe9ef3eff35a5504c351dfe7aeddc0b
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2022-12-29 18:56:44 +0200

    Create parse_integer_float.py

---
+++ parse_integer_float.py
@@ -0,0 +1,13 @@
+def pase(parse):
+    try:
+        print('Your is integer : ', end = '')
+        return int(parse)
+    except ValueError:
+        print('Your is float : ', end = '')
+        return float(parse)
+    # if len(parse) == '.':
+    #    print('Your is float :', parse)
+    # else:
+    #    print('Your is integer :', parse)
+parse = input('Type an integer ot float : ')
+pase(parse)
