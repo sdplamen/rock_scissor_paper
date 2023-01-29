22ef1f25b382d2a16e439b562235c9e63dbffcdc
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-01-29 20:50:09 +0200

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -1,10 +1,10 @@
-# show float number of some percetage
-def calc(perc):
-    print('That\'s your number', a,'\n\tand that\'s its percentage', round(perc, 2), '\n\t\tYour bill is:', round(a + perc, 2))
+# show float number of some percentage
+def calc():
+    perc = a * b / 100
+    print(f'That\'s your number {a} \n\tand that\'s its percentage {round(perc, 2)} \n\t\tYour bill is: {round(a + perc, 2)}')
 a = float(input('Please type a number: '))
 b = float(input('Please type a percentage you want for this number: '))
-perc = a * b / 100
-calc(perc)
+calc()
 
 # Import math library
 import math
