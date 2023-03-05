4bd711de862f8a29a2b1369a4b1895adae09edbc
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-03-05 12:34:30 +0200

    Update calc_discount.js

--- calc_discount.js
+++ calc_discount.js
@@ -7,3 +7,17 @@ function applyDiscount(prices, discount) {
  }
 }
 applyDiscount(prices, discount);
+
+const receipt = [5.99, 6.79, 3.89, 9.99, 2.50];
+const applyDiscount = (total) => total * 0.9;
+const total = (pricesArray) => {
+  let sum = 0;
+  for (let i = 0; i < pricesArray.length; i++) {
+    sum += pricesArray[i];
+  }
+  return sum;
+};
+const subtotal = total(receipt);
+console.log("Subtotal: " + subtotal.toFixed(2));
+const discountedTotal = applyDiscount(subtotal);
+console.log("Discounted Total: " + discountedTotal.toFixed(2));
