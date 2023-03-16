cb8d6af96ee8792a01ab858a7cca5290432dbc8e
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-03-16 15:58:09 +0200

    Create

---
+++ img_select.js
@@ -0,0 +1,14 @@
+function clearBorders() {
+  elements = document.querySelectorAll(".border");
+  for (const element of elements) {
+    element.classList.remove("border");
+  }
+}
+function setMainImage(id) {
+  const mainImage = document.querySelector("#mainImage");
+  const thumbnail = document.querySelector("#thumbnail" + id);
+  const thumbnailSrc = thumbnail.getAttribute("src");
+  mainImage.setAttribute("src", thumbnailSrc);
+  clearBorders();
+  thumbnail.classList.add("border");
+}
