6a980cf8ff2de4c38cb2e5f936f50071c77900a2
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-01 17:44:57 +0300

    Delete qty_multy.js

--- qty_multy.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector("input");
-
-function qtyChanged() {
-  document.querySelector("p").innerHTML = "Total Price: $" + (Number(input.value) * 1.99);
-}
-input.onchange = qtyChanged;
