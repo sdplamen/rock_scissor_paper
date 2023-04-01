077f9ded5a18a6e8e95e36bda3a671a2ef648402
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-01 17:52:33 +0300

    Delete votes.js

--- votes.js
+++
@@ -1,9 +0,0 @@
-let counter = 3;
-function upvote() {
-  counter ++;
-  document.getElementById("votes").innerText = counter + " Votes";
-}
-function downvote() {
-  counter --;
-  document.getElementById("votes").innerText = counter + " Votes";
-}
