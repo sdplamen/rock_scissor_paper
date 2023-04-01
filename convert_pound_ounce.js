aaeed672b1d9fa32296cc473373d4fe44cec3076
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-01 18:22:46 +0300

    Delete convert_pound_ounce.js

--- convert_pound_ounce.js
+++
@@ -1,7 +0,0 @@
-function poundsToOunces (pounds) {
-  const conversion = 16;
-  const output = pounds * conversion;
-  return output + " Ounces";
-}
-const result = poundsToOunces(6);
-console.log(result);
