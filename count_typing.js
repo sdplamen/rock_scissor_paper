a17324af294a32cf89b79eebd2fae9bdd538ba49
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-04-01 13:32:53 +0300

    Delete count_typing.js

--- count_typing.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('input');
-const log = document.getElementById('log');
-
-function updateValue() {
- log.innerText = input.value.length + " Characters";
-}
-input.oninput = updateValue;
