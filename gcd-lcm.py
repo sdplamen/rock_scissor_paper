fb9f8dfed77c7fd621e542b22730a6289b5b3f64
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-08-22 01:15:17 +0300

    Update gcd-lcm.py

--- gcd-lcm.py
+++ gcd-lcm.py
@@ -1,22 +1,27 @@
 # Greatest common divisor (GCD) of two positive integers.
-'''Algorithm: Step 1: Let  x, y be the two numbers
-Step 2: x mod y = R
-Step 3: Let x = y and y = R
-Step 4: Repeat Steps 2 and 3 until x mod y is greater than 0
-Step 5: GCD = x Step 6: Finish'''
+'''Algorithm: Step 1: Let  a, b be the two numbers
+Step 2: a mod b = R
+Step 3: Let a = b and b = R
+Step 4: Repeat Steps 2 and 3 until a mod b is greater than 0
+Step 5: GCD = a Step 6: Finish'''
 
-def gcd(x, y):
-    if y == 0:
-        return x
+def gcd():
+    a = int(input('type a digit for GCD: '))
+    b = int(input('type a digit for GCD: '))
+    print(math.gcd(a, b))
+gcd()
+def gcd(a, b):
+    if b == 0:
+        return a
     else:
-        return gcd(y, (x % y))
-x = 13
-y = 137
-print('GCD of', x, 'and', y, 'is',gcd(x, y))
+        return gcd(b, (a % b))
+a = int(input('type a digit for GCD: '))
+b = int(input('type a digit for GCD: '))
+print('GCD of', a, 'and', b, 'is',gcd(a, b))
 
 # Least common multiple (LCM) of two positive integers.
-def lcm(x, y):
-    return (x / gcd(x, y)) * y
-x = 4
-y = 112
+def lcm(a, b):
+    return (a / gcd(a, b)) * b
+a = int(input('type a digit for GCD: '))
+b = int(input('type a digit for GCD: '))
 print('LCM of', x, 'and', y, 'is', lcm(x, y))
