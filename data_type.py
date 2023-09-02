c1db46d13cba0fed3b772bebdf186c9689025f0c
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-09-02 10:13:30 +0300

    Create data_type.py

---
+++ data_type.py
@@ -0,0 +1,10 @@
+# Data Types
+  # datetime — Basic date and time types
+  # zoneinfo — IANA time zone support
+  # calendar — General calendar-related functions
+  # collections — Container datatypes
+  # collections.abc — Abstract Base Classes for Containers
+  # heapq — Heap queue algorithm
+  # bisect — Array bisection algorithm
+  # array — Efficient arrays of numeric values
+  # weakref — Weak references
