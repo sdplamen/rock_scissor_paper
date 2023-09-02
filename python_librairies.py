167cc2529868d5c4fc478c9b72475390c889b77e
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-09-02 10:07:37 +0300

    Create python_librairies.py

---
+++ python_librairies.py
@@ -0,0 +1,8 @@
+# Numeric and Mathematical Modules
+  # numbers — Numeric abstract base classes
+  # math — Mathematical functions
+  # cmath — Mathematical functions for complex numbers
+  # decimal — Decimal fixed point and floating point arithmetic
+  # fractions — Rational numbers
+  # random — Generate pseudo-random numbers
+  # statistics — Mathematical statistics functions
