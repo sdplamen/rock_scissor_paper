f61e1b9a7431aaf7e0e4cdc2207bf47a0698d06f
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2023-09-22 03:42:25 +0300

    Create color_palette_matplotlib.py

---
+++ color_palette_matplotlib.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt, random as r, numpy as np
+
+# Define the number of colors in the palette
+num_colors = 5
+colormap = ['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Grays', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_grey', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gist_yerg', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'grey', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r']
+# Generate a color palette using a colormap
+cmap = plt.get_cmap(r.choice(colormap))  # You can change with :
+# sequential colormap : 'viridis', 'plasma', 'inferno', 'cividis', 'magma'
+# diverging colormaps : 'coolwarm', 'RdBu', 'seismic'
+# qualitative colormaps : 'Set1', 'Set2', 'Set3', 'Pastel1', 'Pastel2', 'Dark2', 'tab10', 'tab20', 'tab20b', 'tab20c'
+# other notable colormaps : 'rainbow', 'jet', 'nipy_spectral', 'twilight', 'twilight_shifted', 'terrain', 'ocean'
+
+# Create a range of values to represent the palette
+values = np.linspace(0, 1, num_colors)
+
+# Create a synchronized color palette
+colors = [cmap(value) for value in values]
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Plot colored squares to display the palette
+for i, color in enumerate(colors):
+    ax.add_patch(plt.Rectangle((i, 0), 1, 1, color=color))
+
+# Set the x-axis limits and remove y-axis
+ax.set_xlim(0, num_colors)
+ax.set_ylim(0, 1)
+ax.axis('off')
+
+# Show the color palette
+plt.show()
