0e271f1197c37b7a0560827a6d856e94d359b7ff
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2024-03-15 12:28:34 +0200

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -5,15 +5,3 @@ def calc():
 a = float(input('Please type a number: '))
 b = float(input('Please type a percentage you want for this number: '))
 calc()
-
-# Import math library
-import math
-
-# Round a number upward to its nearest integer
-x = math.ceil(10.4)
-
-# Round a number downward to its nearest integer
-y = math.floor(51.6)
-
-print(x)
-print(y)
