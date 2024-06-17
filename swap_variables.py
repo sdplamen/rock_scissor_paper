5f2c955e5faa5ce02a3bca70f82ab688fe9d30ff
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2024-06-17 14:49:42 +0300

    Delete swap_variables.py

--- swap_variables.py
+++
@@ -1,9 +0,0 @@
-def swap():
-    z = x1
-    x = y1
-    y = z
-    print('The value of x after swapping:', x)
-    print('The value of y after swapping:', z)
-x1 = input('Type a variable x to swap :')
-y1 = input('Type a variable y to swap :')
-swap()
